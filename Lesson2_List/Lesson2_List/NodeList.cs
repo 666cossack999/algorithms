bf94e267fe6b4dc0222eec30b2265ac6dc48b1a4
Jackson <dmitrievei92@icloud.com>
2021-04-22 16:49:55 +0300

    update lesson2

---
+++ Lesson2_List/Lesson2_List/NodeList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2_List
+{
+    class NodeList : ILinkedList
+    {
+        List<Node> nodes = new List<Node>();
+        int size = 0;
+        public NodeList()
+        {
+
+        }
+        
+        /// <summary>
+        /// добавляем элемент в конец списка
+        /// </summary>
+        /// <param name="value">элемент</param>
+        public void AddNode(int value)
+        {
+
+            var node = GetLastNode();
+
+            if (node == null)
+            {
+                var newNode = new Node(null, value, null);
+                nodes.Add(newNode);
+                size++;
+            }
+            else
+            {
+                var newNode = new Node(node, value, null);
+                node.NextNode = newNode;
+                nodes.Add(newNode);
+            }             
+
+            
+
+        }
+        /// <summary>
+        /// Ищет последнюю ноду в списке
+        /// </summary>
+        /// <returns>Last Node</returns>
+        public Node GetLastNode()
+        {
+
+            foreach (var node in nodes)
+            {
+                if (node.NextNode == null)
+                {
+                    return node;
+                }                
+            }
+            return null;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lesson2_List/Lesson2_List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson2_List
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            NodeList MyList = new NodeList();
+
+            MyList.AddNode(8);
+            MyList.AddNode(11);
+            MyList.AddNode(15);
+
+        }
+    }
+}
