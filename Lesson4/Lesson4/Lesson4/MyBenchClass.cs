810990e9f9d1e40f72df24d37a5739cac583fd33
Jackson <dmitrievei92@icloud.com>
2021-04-29 15:32:51 +0300

    Lesson4_1Final

---
+++ Lesson4/Lesson4/Lesson4/MyBenchClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Lesson4
+{
+    public class MyBenchClass
+    {
+        public static Random rand = new Random();
+        //глубина списка
+        public static int counts = 10000;
+
+        public string[] myArray = new string[counts];
+
+        /// <summary>
+        /// Заполняет массив случайными числами 0-1000 типа string
+        /// </summary>
+        /// <returns></returns>
+        public string[] RandomArrayPush()
+        {
+            for (int i = 0; i < counts; i++)
+            {
+                myArray[i] = Convert.ToString(rand.Next(0, 1000));
+            }
+            return myArray;
+        }
+
+        public HashSet<string> myHashSet = new HashSet<string>();
+
+        /// <summary>
+        /// Заполняет Хэш случайными числами 0-1000 типа string
+        /// </summary>
+        /// <returns></returns>
+        public HashSet<string> RandomHashPush()
+        {
+            myArray = RandomArrayPush();
+            for (int i = 0; i < counts; i++)
+            {
+                var number = Convert.ToString(rand.Next(0, 1000));
+                myHashSet.Add(number);
+            }
+            return myHashSet;
+        }
+        /// <summary>
+        /// Поиск значения в ХэшТаблице
+        /// </summary>
+        /// <param name="a">значение</param>
+        /// <param name="Hash">Хэш-Таблица</param>
+        /// <returns>true/false</returns>
+        public bool HashSearch(string a, HashSet<string> Hash)
+        {
+            return Hash.Contains(a);
+        }
+        /// <summary>
+        /// Поиск значения в массиве
+        /// </summary>
+        /// <param name="a">значение</param>
+        /// <param name="array">массив</param>
+        /// <returns>true/false</returns>
+        public bool ArraySearch(string a, string[] array)
+        {
+            for (int i = 0; i < counts; i++)
+            {
+                if (array[i] == a)
+                    return true;
+            }
+            return false;
+        }
+
+        [Benchmark]
+        public void TestHash()
+        {
+            HashSearch(Convert.ToString(25), myHashSet);
+        }
+        [Benchmark]
+        public void TestArray()
+        {
+            ArraySearch(Convert.ToString(25), myArray);
+        }
+    }
+}
---
+++ Lesson4/Lesson4/Lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var reuslt = BenchmarkRunner.Run<MyBenchClass>();
+
+            Console.WriteLine("Здарова______________________");
+            Console.WriteLine(reuslt.Table);
+            
+        }
+    }
+}
